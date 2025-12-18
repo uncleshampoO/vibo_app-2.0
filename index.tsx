dac018bd9f4aa56d3da480fb0ed80e304b0750a9
uncleshampoO <shampyn9@gmail.com>
2025-12-18 16:43:36 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,12 @@
+console.log("Vibo App: Запуск index.tsx..."); // ЭТА СТРОКА ПОМОЖЕТ НАМ УВИДЕТЬ СТАРТ
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("ОШИБКА: Не найден root элемент!");
   throw new Error("Could not find root element to mount to");
 }
 
